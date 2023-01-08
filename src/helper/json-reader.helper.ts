10fdcecd798702e6bd94b9cb15f6bd46fe86be3b
Bipin Dasan <bipindigits@gmail.com>
2023-01-08 02:55:24 -0600

    fspath to path changes for mac and windows

--- src/helper/json-reader.helper.ts
+++ src/helper/json-reader.helper.ts
@@ -3,7 +3,7 @@ import * as vscode from "vscode";
 export class JsonReader {
     static read<T>(context: vscode.ExtensionContext, ...pathSegments: string[]): T {
         const uri = vscode.Uri.joinPath(context.extensionUri, ...pathSegments);
-        let jsonObject = require(uri.path);
+        let jsonObject = require(uri.fsPath);
         return jsonObject;
     }
 }
